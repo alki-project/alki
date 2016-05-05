05369ba63ffde3d704d31c5ea1c66beb91306897
Matt Edlefsen <matt@xforty.com>
2016-05-05 10:36:27 -0700

    Add better autodetection code

--- lib/alki/setup.rb
+++ lib/alki/setup.rb
@@ -1,6 +1,18 @@
-require 'bundler/setup'
 require 'alki/base'
-app = Alki::StandardApplication.new Bundler.root
-Alki.define_singleton_method :app do
-  app
+module Alki
+  @apps = {}
+  def self.app
+    old_dir = caller_locations(1,1)[0].absolute_path
+    dir = File.dirname(old_dir)
+    until dir == old_dir || File.exists?(File.join(dir,'Gemfile'))
+      setup_bundler = true
+      old_dir = dir
+      dir = File.dirname(old_dir)
+    end
+    if dir == old_dir
+      raise "Couldn't find app root"
+    end
+    require 'bundler/setup' if setup_bundler
+    @apps[dir] ||= Alki::StandardApplication.new dir
+  end
 end

027fddfd76fa2b675c7a2e7165ff476ec63934ff
Matt Edlefsen <matt.edlefsen@gmail.com>
2016-12-25 22:36:57 -0800

    Remove reloadable and entrypoint value helpers

--- lib/alki/execution/value_helpers.rb
+++ lib/alki/execution/value_helpers.rb
@@ -10,14 +10,6 @@ module Alki
       def delegate_overlay(obj,overlay,**args)
         Alki::OverlayDelegator.new(obj,overlay,args)
       end
-
-      def entrypoint(klass,*args, reloadable: false)
-        klass.new *args.map {|a| reloadable ? self.reloadable(a) : lookup(a) }
-      end
-
-      def reloadable(path)
-        Alki::ReloadableDelegator.new(root.assembly_instance,meta[:building],path)
-      end
     end
   end
 end
